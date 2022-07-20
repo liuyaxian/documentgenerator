68f969515ac796c55b011dbd2d6fc97fcabcf012
liuyaxian <liuyx@jjmmw.com>
2022-07-20 16:53:15 +0800

    add 私募接口

--- src/main/java/service/私募改版/Test5291公募获取单只产品的最新公告列表.java
+++ src/main/java/service/私募改版/Test5291公募获取单只产品的最新公告列表.java
@@ -3,28 +3,27 @@ package service.私募改版;
 import net.sf.json.JSONObject;
 import service.DocumentGeneratorServiceImpl;
 
-public class Test5273客户产品维度的持仓 extends DocumentGeneratorServiceImpl {
+public class Test5291公募获取单只产品的最新公告列表  extends DocumentGeneratorServiceImpl {
 
-    private static final  String bizcode = "5273";
-    private static final  String bizcodeDesc = "客户产品维度的持仓";
+    private static final  String bizcode = "5291";
+    private static final  String bizcodeDesc = "公募获取单只产品的最新公告列表";
 
     public static void main(String[] args) throws Exception {
-        // 需要鉴权的接口 custNo 不为为空 1001883450 1001883516  1001883450  预约赎回 1001883979
-        String custNo =  "1001883979";
+        // 需要鉴权的接口 custNo 不为为空 1001883450 1001883516
+        String custNo =  "1001883450";
         // 需要数据交易密码的接口
         String tradeacco = "";
         // 交易密码 111111
         String passwd = "";
-        String isAuth = "1";
         JSONObject bodyJson =  setBody(bizcode, custNo);
-        // 1001546198	1001883763
+        String isAuth = "";
         requestUrl(custNo, tradeacco, passwd, bizcode, bizcodeDesc, bodyJson, isAuth);
-
     }
 
     public static  JSONObject setBody(String bizcode, String custNo){
         JSONObject bodyJson = new JSONObject();
-        bodyJson.put("zlbalancetype", "6");
+        bodyJson.put("fundid", "000771");
+
         return bodyJson;
     }
 }
